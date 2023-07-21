848870e249395e27c06dea46574cfd24f9c244c5
Shalaka Devarde <shalakadevarde14@gmail.com>
2023-07-21 16:01:33 +0530

    registration api

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const appModules = express.Router();
-
-const sampleModules = require("app_modules/sample_module/routes");
-const customerModules = require("modules/customer/routes");
-
-// module_name: sample
-// module_route: /sample
-// module_description:
-//      handles routes related to sample module
-appModules.use("/sample", sampleModules)
+const customer = require("../modules/customer/routes");
 
 // module_name: customer
 // module_route: /customer
 // module_description:
 //      handles routes related to customer module
-appModules.use("/customer", customerModules)
+appModules.use("/customer", customer)
 
 module.exports = appModules;

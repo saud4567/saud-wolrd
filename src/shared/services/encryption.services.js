5920a1e906a5b57794d4e4d452799c6bebdfd882
Mohd Adnan Ansari <adnan.adnan@techstalwarts.com>
2023-08-08 20:08:04 +0530

    Update encryption.services.js

--- src/shared/services/encryption.services.js
+++ src/shared/services/encryption.services.js
@@ -17,39 +17,39 @@ const encryptionIV = crypto
 
 // service_name: encryptData
 // service_description: data encryption
-encryptionServices.encryptData = (data) => {
+// encryptionServices.encryptData = (data) => {
 
-	Object.keys(data).forEach((key) => {
+// 	Object.keys(data).forEach((key) => {
 
-		if (sharedConstants.masterConstants.ENCRYPTION_DECRYPTION_KEYS.indexOf(key) !== -1) {
+// 		if (sharedConstants.masterConstants.ENCRYPTION_DECRYPTION_KEYS.indexOf(key) !== -1) {
 
-			const cipher = crypto.createCipheriv('aes-256-cbc', aeskey, encryptionIV)
-			data[key] = cipher.update(data[key], 'utf8', 'base64');
-			data[key] += cipher.final('base64');
-		}
+// 			const cipher = crypto.createCipheriv('aes-256-cbc', aeskey, encryptionIV)
+// 			data[key] = cipher.update(data[key], 'utf8', 'base64');
+// 			data[key] += cipher.final('base64');
+// 		}
 
-	});
+// 	});
 
-	return data;
-};
+// 	return data;
+// };
 
 // service_name: decryptData
 // service_description: data decyption
-encryptionServices.decryptData = (data) => {
+// encryptionServices.decryptData = (data) => {
 
-	data.map((d) => {
-		Object.keys(d).forEach((key) => {
+// 	data.map((d) => {
+// 		Object.keys(d).forEach((key) => {
 
-			if (sharedConstants.masterConstants.ENCRYPTION_DECRYPTION_KEYS.indexOf(key) !== -1) {
+// 			if (sharedConstants.masterConstants.ENCRYPTION_DECRYPTION_KEYS.indexOf(key) !== -1) {
 
-				let decipher = crypto.createDecipheriv('aes-256-cbc', aeskey, encryptionIV);
-				d[key] = decipher.update(d[key], 'base64', 'utf8');
-				d[key] += decipher.final('utf8');
-			}
+// 				let decipher = crypto.createDecipheriv('aes-256-cbc', aeskey, encryptionIV);
+// 				d[key] = decipher.update(d[key], 'base64', 'utf8');
+// 				d[key] += decipher.final('utf8');
+// 			}
 
-		});
-	});
-	return data;
-};
+// 		});
+// 	});
+// 	return data;
+// };
 
 module.exports = encryptionServices;

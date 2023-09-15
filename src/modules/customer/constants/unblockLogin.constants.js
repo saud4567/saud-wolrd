8a84eef190190aa11cd26861c4eff91cad57708c
shalakadevarde <shalaka.devarde@techstalwarts.com>
2023-09-15 18:41:46 +0530

    test cases for unblock login api

--- src/modules/customer/constants/unblockLogin.constants.js
+++ src/modules/customer/constants/unblockLogin.constants.js
@@ -23,4 +23,15 @@ module.exports = {
       message: "Credentials Not Set For This User",
     },
   },
+  TEST_CONSTANT: {
+    PAYLOAD: {
+      username: "shalaka.devarde+11@techstalwarts.com",
+    },
+    INVALID_PAYLOAD_WITH_MISSING_KEYS: {
+      // username: "shalaka.devarde+11@techstalwarts.com",
+    },
+    INVALID_PAYLOAD_WITH_EMPTY_VALUES: {
+      username: "",
+    },
+  },
 };
